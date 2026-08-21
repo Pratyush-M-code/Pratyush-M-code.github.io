51e77b3ebe8867f7bbc669ed51f0e99ff44f81af
Pratyush <pratyush.m110@gmail.com>
2026-08-21 20:27:33 +0530

    Added base HTML, CSS files and made pixelateCorner Function.
    Worked on the base of the CSS file,fixing the colour scheme and making a proper base for future stuff. Made the basic website HTML and finalized the ids and classes of the divs (yet to be added). And made a pixlateCorner function to make the divs look nicer and more on theme.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 console.log('Happy developing ✨')
+console.log("IM TRYING TO SEE IF WORKS DAMMIT")
+
+/*Pixelated borders*/
+function pixelateCorner(element, options={}){
+    const css_values = getComputedStyle(element);
+    const pixelStep = parseFloat(css_values.getPropertyValue("--ps"));
+    const stepCount = parseInt(css_values.getPropertyValue("--steps"));
+
+    let x = 0;
+    let y = pixelStep*stepCount;
+    const polygonPath = [[x,y]];/*basically the first [] defines the total path and each nested [] is a point*/
+    let horizontal = true;
+
+    for(let i = 0; i < stepCount * 2; i = i + 1) {
+        if(horizontal) {
+            x = x + pixelStep
+        } else {
+           y = y - pixelStep
+        }
+        polygonPath.push([x,y]);
+        horizontal = !horizontal
+    }
+
+    const start = (val) => (val === 0 ? "0px":`${val}px`)
+    const end = (val) => (val === 0 ? "100%":`calc(100% - ${val}px)`)
+    /*top-t + left-l + corner*/
+    const tlcorner = [...polygonPath].map(([X,Y])=>`${start(X)} ${start(Y)}`);
+    const trcorner = [...polygonPath].reverse().map(([X,Y])=>`${end(X)} ${start(Y)}`);
+    const brcorner = [...polygonPath].map(([X,Y])=>`${end(X)} ${end(Y)}`);
+    const blcorner = [...polygonPath].reverse().map(([X,Y])=>`${start(X)} ${end(Y)}`);
+    const allPoints = [
+        ...tlcorner,
+        ...trcorner,
+        ...brcorner,
+        ...blcorner,
+    ];
+
+    element.style.clipPath = `polygon(${allPoints.join(', ')})`;
+
+}
+
+document.querySelectorAll('.pixel-corners').forEach((element) => pixelateCorner(element));
